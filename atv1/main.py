192e17515670876712d85f94ec263e2447eea725
castro <antoniog_castro@hotmail.com>
2024-08-31 23:58:26 -0300

    fix: alteração na estrutura do codigo com fins de simplificação

--- atv1/main.py
+++ atv1/main.py
@@ -1,136 +1,16 @@
 import time
 import sys
 import random
+from algOrd import counting_sort, heapify, heap_sort, merge_sort, insertion_sort, bubble_sort, partition, quick_sort
 
-# Defina um novo limite de recursãO
+# limite de recursão
 sys.setrecursionlimit(150000)
-
-
-# ALGORITMO DE ORDENAÇÃO COUNTINGSORT
-def counting_sort(arr):
-    max_val = max(arr)
-    min_val = min(arr)
-    range_val = max_val - min_val + 1
-    count = [0] * range_val
-    output = [0] * len(arr)
-
-    for num in arr:
-        count[num - min_val] += 1
-
-    for i in range(1, len(count)):
-        count[i] += count[i-1]
-
-    for num in arr:
-        output[count[num - min_val] - 1] = num
-        count[num - min_val] -= 1
-
-    for i in range(len(arr)):
-        arr[i] = output[i]
-
-# ALGORITMO DE ORDENAÇÃO HEAPSORT
-def heapify(arr, n, i):
-    maior = i
-    esquerda = 2 * i + 1
-    direita = 2 * i + 2
-
-    if esquerda < n and arr[i] < arr[esquerda]:
-        maior = esquerda
-
-    if direita < n and arr[maior] < arr[direita]:
-        maior = direita
-
-    if maior != i:
-        arr[i], arr[maior] = arr[maior], arr[i]
-        heapify(arr, n, maior)
-
-def heap_sort(arr):
-    n = len(arr)
-
-    for i in range(n//2 - 1, -1, -1):
-        heapify(arr, n, i)
-
-    for i in range(n-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]
-        heapify(arr, i, 0)
-
-# ALGORITMO DE ORDENAÇÃO MERGESORT
-def merge_sort(arr):
-    if len(arr) > 1:
-        meio = len(arr)//2
-        metade_esquerda = arr[:meio]
-        metade_direita = arr[meio:]
-
-        merge_sort(metade_esquerda)
-        merge_sort(metade_direita)
-
-        i = j = k = 0
-
-        while i < len(metade_esquerda) and j < len(metade_direita):
-            if metade_esquerda[i] < metade_direita[j]:
-                arr[k] = metade_esquerda[i]
-                i += 1
-            else:
-                arr[k] = metade_direita[j]
-                j += 1
-            k += 1
-
-        while i < len(metade_esquerda):
-            arr[k] = metade_esquerda[i]
-            i += 1
-            k += 1
-
-        while j < len(metade_direita):
-            arr[k] = metade_direita[j]
-            j += 1
-            k += 1
-
-# ALGORITMO DE ORDENAÇÃO INSERTIONSORT
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        chave = arr[i]
-        j = i-1
-        while j >= 0 and arr[j] > chave:
-            arr[j+1] = arr[j]
-            j -= 1
-        arr[j+1] = chave
-
-# ALGORITMO DE ORDENAÇÃO BUBBLESORT
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n-1):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-
-def partition(arr, baixo, alto):
-    i = baixo - 1
-    pivo = arr[alto]
-
-    for j in range(baixo, alto):
-        if arr[j] < pivo:
-            i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-
-    arr[i+1], arr[alto] = arr[alto], arr[i+1]
-    return i+1
-
-# ALGORITMO DE ORDENAÇÃO QUICKSORT
-def quick_sort(arr, baixo, alto):
-    if baixo < alto:
-        pi = partition(arr, baixo, alto)
-
-        quick_sort(arr, baixo, pi-1)
-        quick_sort(arr, pi+1, alto)
-
-
 
 # entrada dos valores iniciais, pedidos no enunciado
 inc = int(input("tamanho incial do vetor de entrada: "))
 fim = int(input("tamanho final do vetor de entrada:  "))
 stp = int(input("valor do 'stepper': "))
 rps = int(input("número de repetições: "))
-
-
 
 # calculo do tempo do vetor RANDOM
 def my_random(inc, fim, stp, rps):
@@ -192,7 +72,6 @@ def my_random(inc, fim, stp, rps):
 
         print(f"{inc}  {(tempoBub/rps):.6f}     {(tempoIns/rps):.6f}     {(tempoMer/rps):.6f}     {(tempoHeap/rps):.6f}    {(tempoQuick/rps):.6f}     {(tempoCount/rps):.6f}")
         inc = inc + stp
-
 
 # calculo do tempo do vetor REVERSE
 def reverse(inc, fim, stp, rps):
