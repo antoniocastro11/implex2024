793abcf3eace67ef6bfbd7ba98be6833ba77bd3d
jaopcruz <joaopcruz2407@gmail.com>
2024-10-21 22:26:29 -0400

    adição de texto e main da atividade 2

---
+++ atv2/main.py
@@ -0,0 +1,11 @@
+'''inc = int(input("tamanho incial do vetor de entrada: "))
+
+fim = int(input("tamanho final do vetor de entrada:  "))
+while fim <= inc:
+    print("O valor final do vetor não pode ser menor ou igual ao valor inicial")
+    fim = int(input("novo valor final do vetor de entrada: "))
+
+stp = int(input("valor do 'stepper': "))
+while stp == 0:
+    print("O valor do 'stepper' não pode ser 0")
+    stp = int(input("novo valor do 'stepper': "))'''
